67f864d6d70239cc6579994007415a3e96d0b31f
tungbk <tungbk@itsol.vn>
2021-11-05 09:23:14 +0700

    first commit

---
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.example.employee.repository;
+
+import com.example.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployee(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
